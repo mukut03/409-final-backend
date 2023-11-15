5b21de4d1de3dd01976354291f0e09ed7eed7bf5
mukut03 <ilovecars252003mukut@gmail.com>
2023-11-15 07:44:37 -0500

    5 user interactions supported

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ passport.deserializeUser((id, done) => {
 
 // Authentication routes
 app.get('/login', passport.authenticate('spotify', {
-    scope: ['user-read-email', 'user-read-private'],
+    scope: ['user-read-email', 'user-read-private', 'ugc-image-upload', 'playlist-modify-private', 'playlist-modify-public'],
     showDialog: true
 }));
 
@@ -71,7 +71,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/playlists', (req, res) => {
+app.get('/playlists', (req, res) => { // user interaction 1
     spotifyApi.getUserPlaylists()
         .then(data => {
             // Extracting playlist names
@@ -85,6 +85,91 @@ app.get('/playlists', (req, res) => {
         })
         .catch(err => {
             res.status(400).send(`Error fetching playlists: ${err}`);
+        });
+});
+
+app.get('/search-playlists', (req, res) => { // user interaction 2
+    const { query } = req.query;  // Extract the search query from the request query string
+
+    if (!query) {
+        return res.status(400).send('A search query is required');
+    }
+
+    spotifyApi.searchPlaylists(query)
+        .then(data => {
+            res.json(data.body.playlists.items);  // You can modify the response as needed
+        })
+        .catch(err => {
+            res.status(400).send(`Error searching playlists: ${err}`);
+        });
+});
+
+app.post('/select-playlist', (req, res) => { // user interaction 3
+    const { playlistId } = req.body;
+
+    if (!playlistId) {
+        return res.status(400).send('Playlist ID is required');
+    }
+
+    // Here, you can handle the playlist selection, e.g., fetching tracks, storing the ID, etc.
+    console.log(`Playlist selected: ${playlistId}`);
+    // For example, you could store the playlist ID in a variable or a database
+    // and then use it to fetch the tracks or for other processing.
+
+    res.send(`Playlist ${playlistId} selected`);
+});
+
+
+app.get('/playlist-tracks/:playlistId', (req, res) => { // user interaction 3
+    const playlistId = req.params.playlistId;
+
+    spotifyApi.getPlaylistTracks(playlistId)
+        .then(data => {
+            // Extracting album art URLs from each track
+            const albumArtUrls = data.body.items.map(item => {
+                // Some tracks might not have album art, so we check for it
+                return item.track.album.images.length > 0 ? item.track.album.images[0].url : null;
+            }).filter(url => url !== null); // Filtering out tracks without album art
+
+            res.json(albumArtUrls); // Sending only the album art URLs to the frontend
+        })
+        .catch(err => {
+            res.status(400).send(`Error fetching playlist tracks: ${err}`);
+        });
+});
+
+app.post('/set-playlist-image/:playlistId', (req, res) => { // user interaction 4
+    const playlistId = req.params.playlistId;
+    const { imageBase64 } = req.body; // Assuming the image is sent as a base64 encoded string
+
+    if (!playlistId || !imageBase64) {
+        return res.status(400).send('Playlist ID and image data are required');
+    }
+
+    // Spotify API call to set the playlist image goes here
+    // Spotify requires the image to be in JPEG format, encoded in base64
+    spotifyApi.uploadCustomPlaylistCoverImage(playlistId, imageBase64)
+        .then(data => {
+            res.send('Playlist image updated successfully');
+        })
+        .catch(err => {
+            res.status(400).send(`Error setting playlist image: ${err}`);
+        });
+});
+
+app.post('/follow-playlist', (req, res) => { // user interaction 5
+    const { playlistId } = req.body;
+
+    if (!playlistId) {
+        return res.status(400).send('Playlist ID is required');
+    }
+
+    spotifyApi.followPlaylist(playlistId)
+        .then(() => {
+            res.send(`Successfully followed playlist with ID: ${playlistId}`);
+        })
+        .catch(err => {
+            res.status(400).send(`Error following playlist: ${err}`);
         });
 });
 
