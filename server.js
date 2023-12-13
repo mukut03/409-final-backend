174df2792102ee53b698c262e04012a8607593d1
mukut03 <ilovecars252003mukut@gmail.com>
2023-12-13 09:11:12 -0600

    fixing s3 CORS issue

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ const AWS = require('aws-sdk');
 
 
 AWS.config.update({
-    accessKeyId: 'YOUR_ACCESS_KEY_ID',
-    secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
-
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
   });
   
   const s3 = new AWS.S3()
