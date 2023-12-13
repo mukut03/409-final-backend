df0ca355d64cb99d684d0497f196495130b8207e
mukut03 <ilovecars252003mukut@gmail.com>
2023-12-13 08:53:06 -0600

    update CORS 5

--- server.js
+++ server.js
@@ -33,13 +33,10 @@ const AWS = require('aws-sdk');
 AWS.config.update({
     accessKeyId: 'YOUR_ACCESS_KEY_ID',
     secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
-    region: 'YOUR_REGION',
+
   });
   
-  const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-  })
+  const s3 = new AWS.S3()
 
 const ddb = new AWS.DynamoDB.DocumentClient();
 
